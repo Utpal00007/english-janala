054c759f82906332d3d42fbed1727a02e85f3b03
Utpal <2022746@iub.edu.bd>
2026-01-09 20:25:03 +0600

    added lear Learn Vocabularies  Section and js added card of Learn Vocabularies  Section

---
+++ script/index.js
@@ -0,0 +1,56 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all").then((res) =>
+    res.json().then((json) => displayLesson(json.data))
+  );
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div
+        class="bg-white rounded-xl shadow-sm text-center py-20 px-5 space-y-4"
+      >
+        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <p class="semi-bold">Meaning / Programming</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between items-center mt-5">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>`;
+    wordContainer.appendChild(card);
+  });
+};
+
+const displayLesson = (lessons) => {
+  const levelContainer = document.getElementById("Level-container");
+  levelContainer.innerHTML = "";
+  console.log(lessons);
+
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+
+    btnDiv.innerHTML = ` <button  onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
+                  ><i class="fa-solid fa-book-open"></i>Lesson -  ${lesson.level_no}
+                </button>`;
+
+    levelContainer.appendChild(btnDiv);
+  }
+};
+
+loadLessons();
