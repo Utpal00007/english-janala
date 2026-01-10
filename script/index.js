8e8ba992221b644bb5e1fb3361fa2b9ab093c405
Utpal <2022746@iub.edu.bd>
2026-01-10 12:04:31 +0600

    commit for Handle Errors , empty lessons for Better UX

--- script/index.js
+++ script/index.js
@@ -15,15 +15,33 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length == 0) {
+    wordContainer.innerHTML = `<div
+        class="text-center  col-span-full rounded-xl space-y-6 font-bangla py-10"
+      >
+      <img   class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-xl font-medium text-gray-400">
+         এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>`;
+  }
+
   words.forEach((word) => {
     const card = document.createElement("div");
     card.innerHTML = `
     <div
         class="bg-white rounded-xl shadow-sm text-center py-20 px-5 space-y-4"
       >
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <h2 class="font-bold text-2xl">${
+          word.word ? word.word : "Word Not Found"
+        }</h2>
         <p class="semi-bold">Meaning / Programming</p>
-        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="text-2xl font-medium font-bangla">"${
+          word.meaning ? word.meaning : "Meaning Not Found"
+        } / ${
+      word.pronunciation ? word.pronunciation : "Pronunciation Not Found"
+    }"</div>
         <div class="flex justify-between items-center mt-5">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-info"></i>
